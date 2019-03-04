d7ab5bfb563543c5b328f365de86e073f062494a
松田幸介 <darby8928@gmail.com>
2019-03-04 20:17:04 +0900

    ちょっと基盤を作成

---
+++ rhythmGame/Assets/Scripts/ButtonScripts.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScripts : MonoBehaviour {
+
+    [SerializeField] GameObject startButton;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void GameStart()
+    {
+        startButton.SetActive(false);
+        //レーンをアクティブ化
+        //該当の譜面をサーチ
+        //ゲームスタート
+    }
+}
